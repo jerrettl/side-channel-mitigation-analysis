e28b3e83a0c6e8d0822fe55b54de754e656011ad
Jerrett Longworth <jerrett7@gmail.com>
2024-04-11 21:34:28 -0400

    Add blender and 7zip tests

--- tester.sh
+++ tester.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 
+if [[ -z "$BLENDER_BENCHMARK_LOCATION" ]]; then
+	echo "You should specify the location of your Blender Open Data Benchmark script."
+	echo "Example: BLENDER_BENCHMARK_LOCATION=\"/path/to/the/script/benchmark-launcher-cli\" $0"
+	echo
+	echo "Note: Before running, you should set up the \`benchmark-launcher-cli\` script with the following:"
+	echo "        ./benchmark-launcher-cli blender download 4.0.0"
+	echo "        ./benchmark-launcher-cli scenes download monster --blender-version 4.0.0"
+	echo "(You may need to move to the folder that contains the script first.)"
+	exit 1
+fi
+
+
 echo "Running tests..."
 exec 3>&1
+
 
 # Sysbench
 echo "==> Sysbench"
@@ -28,6 +41,27 @@ passmark_memory_read="$(cat results_memory.yml | grep 'ME_READ_S' | cut -d' ' -f
 echo "Memory read: $passmark_memory_read"
 passmark_memory_write="$(cat results_memory.yml | grep 'ME_WRITE' | cut -d' ' -f4)"
 echo "Memory write: $passmark_memory_write"
+
+
+
+echo
+echo "==> Blender Open Data Benchmark"
+blender="$($BLENDER_BENCHMARK_LOCATION benchmark --blender-version 4.0.0 --device-type CPU --json monster | tee /dev/fd/3)"
+
+blender_result="$(echo "$blender" | jq .[0].stats.total_render_time)"
+echo "Blender result: $blender_result"
+
+
+
+echo
+echo "==> 7zip"
+zip="$(7z b | tee /dev/fd/3)"
+
+zip_result_compress="$(echo "$zip" | grep -E 'Avr' | awk '{print $3}')"
+echo "7zip compress: $zip_result_compress"
+zip_result_decompress="$(echo "$zip" | grep -E 'Avr' | awk '{print $7}')"
+echo "7zip decompress: $zip_result_decompress"
+
 
 
 
